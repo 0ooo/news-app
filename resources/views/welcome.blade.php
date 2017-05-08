a2a1095d915075cb6affb9dc69bbbd419cd8e31f
Pavel Gontarenko <pavgont@gmail.com>
2017-05-08 21:42:16 +0300

    Убрал лишние ссылки с главной

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,14 +81,11 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                @if (Auth::check())
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('news.index') }}">News</a>
                 </div>
+                @endif
             </div>
         </div>
     </body>
