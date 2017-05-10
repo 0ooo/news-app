3f2dcccd299674b5f55a2eabb3cad1b5e2c3e639
Pavel Gontarenko <pavgont@gmail.com>
2017-05-10 19:37:25 +0300

    Удалил пустую строку

---
+++ app/Http/Middleware/GateMiddleware.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pavel
+ * Date: 10.05.2017
+ * Time: 17:50
+ */
+
+namespace App\Http\Middleware;
+
+
+class GateMiddleware
+{
+
+}
---
+++ app/Http/Requests/NewsUpdateRequest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pavel
+ * Date: 10.05.2017
+ * Time: 12:48
+ */
+
+namespace App\Http\Requests;
+
+
+class NewsUpdateRequest
+{
+
+}
