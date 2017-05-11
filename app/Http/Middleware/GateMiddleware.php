da3b30696c19cc50313742582dbbdd60548fcbb0
Pavel Gontarenko <pavgont@gmail.com>
2017-05-11 06:17:32 +0300

    Middleware для проверки ролей

--- app/Http/Middleware/GateMiddleware.php
+++ app/Http/Middleware/GateMiddleware.php
@@ -1,15 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Pavel
- * Date: 10.05.2017
- * Time: 17:50
- */
 
 namespace App\Http\Middleware;
-
+use Closure;
 
 class GateMiddleware
 {
-
+    /**
+     * Проверка роли пользователя на доступ к определенным разделам
+     *
+     * @param $request
+     * @param Closure $next
+     * @param $roles
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $roles)
+    {
+        $roles = explode('|', $roles);
+        if (auth()->user()->roles->pluck('slug')->intersect($roles)->count() > 0) {
+            return $next($request);
+        }else {
+            abort(403);
+        }
+    }
 }
